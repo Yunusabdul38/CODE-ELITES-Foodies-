fd01cc51d1a19cb707de814f20bedf7b6571de5f
Yunus AbdulMajid <Yunusabdulmajidyunus38@gmail.com>
2023-09-03 13:56:41 +0100

    transition added to navigaton

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -18,9 +18,9 @@ function NavigationBar({handlePage,navbar}) {
     setShowMenu(false)
   }
   return (
-    <nav className="py-0 px-8 bg-[#ffe227] z-10 fixed top-0 w-full flex justify-between flex-col md:flex-row items-start md:items-center pb-3 md:py-3">
+    <nav className={`py-0 px-8 bg-[#ffe227] z-10 fixed top-0 w-full flex justify-between flex-col md:flex-row items-start md:items-center pb-3 md:py-3 md:h-fit transition-all ease-linear ${showMenu?"h-2/5":"h-14"}`}>
       <h1 className="font-medium text-4xl font-[ 'cursive']">Foodies</h1>
-      <ul className={`flex justify-between capitalize w-1/2 cursor-pointer text-xl flex-col md:flex-row pb-3 md:pb-0 ${!showMenu?"hidden md:flex":""}`}>
+      <ul className={`flex justify-between capitalize w-1/2 cursor-pointer text-xl flex-col md:flex-row pb-3 md:pb-0 ${!showMenu?"invisible md:visible md:flex":""}`}>
         {navlist.map((data,index)=> <NavLi value={data} handlePage={handlePage} navbar={navbar} key={index} showMenuHandler={showMenuHandler}/>)}
       </ul>
       <span className="absolute block top-[10px] text-2xl right-[30px] md:hidden" onClick={() => setShowMenu((prev) => !prev)}>
--- src/components/NavLi.jsx
+++ src/components/NavLi.jsx
@@ -5,7 +5,7 @@ function NavLi({ value, handlePage, navbar, showMenuHandler}) {
     showMenuHandler()
   }
   const currentPage = value === navbar
-  return <li onClick={navHandler} className={currentPage?"trnslate-y-[-5px] ml-4 md:ml-0 text-white transition-all ease-linear p-[5px] w-1/3 text-center rounded-md bg-[#393e46]":""}>
+  return <li onClick={navHandler} className={currentPage?"trnslate-y-[-5px] ml-4 md:ml-0 text-white transition-all ease-linear p-[5px] w-full max-w-[100px] text-center rounded-md bg-[#393e46]":""}>
   {value}
 </li>;
 }
